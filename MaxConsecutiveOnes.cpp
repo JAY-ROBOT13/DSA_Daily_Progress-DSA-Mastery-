08130ddac8a04f0fc3bac30f6a87f348a74841ef
Jay Prakash Jha <prakashk60843@gmail.com>
2026-06-10 22:44:44 +0530

    Add files via upload
    There complexity is: O(n)
    and there space complexity is : O(1)

--- MaxConsecutiveOnes.cpp
+++ MaxConsecutiveOnes.cpp
@@ -1,36 +1,41 @@
-// Topic is: Max Consecutive Ones
-//Given a binary array nums, return the maximum number of  consecutive 1's in the array
-//Input: nums=[1,1,0,1,1,1]  and output: 3
+//485.Max Consecutive Ones:
+//Given a binary array nums,return the maximum number of consecutive 1's in the array
+//e.g., Input: nums=[1,1,0,1,1,1]
+//Output:3
+//Explanation: The first two digits or the last three digits are consecutive 1s.The maximum number of consecutive 1s is 3.
 
 #include<iostream>
 #include<vector>
 using namespace std;
 
-class solution{                                 //creating a class named solution
+class solution{
     public:
-    int findmaxconsecutiveones(std::vector<int>&  nums){
-        int count=0;                           //initializing a variable count to keep track the number of consecutive ones
-        int maxcount=0;             //initializing a variable maxcount to keep track the maximum number of consecutive ones
+    int findmaxconsecutive(vector<int>&nums){
+        int count=0;
+        int maxcount=0;
+
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
-                count++;
+                count++;                    //increase when value come to 1
+                maxcount=max(maxcount,count);
             }
             else{
-                maxcount=max(maxcount,count);      //updating the maxcount variable with the maximum of the current maxcount and count
                 count=0;
             }
-            }
-            maxcount=max(maxcount,count);     
-            return maxcount;
         }
-    };
-//main function to test the deriven class
-int main(){
-    std::vector<int> nums={1,1,0,1,1,1};
+        return maxcount;
+    }
+};
 
-    //creating an object of the solution class
+//main function
+int main(){
+    vector<int> nums={1,1,0,1,1,1};
     solution s;
-    int result=s.findmaxconsecutiveones(nums);
-    std::cout<<"The maximum number of consecutive ones is:"<<result<<std::endl;
-    return 0;
-}      
+
+    // s.findmaxconsecutive(nums);
+    cout<<"Maximum consecutive Ones: "<< s.findmaxconsecutive(nums);
+
+    //for loop is used for iteration
+    // for(int i=0;i<nums.size();i++){
+    //     cout<<nums[i]<<" ";
+    }
