b2e1edc886234dd7a21e9c977f62df9955d2e844
Jay Prakash Jha <prakashk60843@gmail.com>
2026-06-14 21:58:01 +0530

    Add files via upload
    Time Complexity : O(n)      because due to one time traversal
    Space Complexity: O(1)    no extra space using

--- TwoSumII.cpp
+++ TwoSumII.cpp
@@ -1,7 +1,12 @@
-//Topic is : 167.Two Sum ii - Input Array is sorted
-//Given a 1-indexed array of integers "numbers" that is already sorted in non-decreasing order,find two numbers such that they add up to a specific "target" number. Let these two numbers be numbers[index1]  and numbers[index2] where "1<=index1<index2<=numbers.length" 
-// Return the indices of the two numbers index2 and index2 each incremented by one as an integer array [index1,index2] of length2.
-//e.g., Input: numbers=[2,7,11,15], target=9   , output:[1,2]
+//167.TWO SUM II-INPUT ARRAY IS SORTED:
+//GIVEN A 1-INDEXED ARRAY OF INTEGERS "NUMBERS" THAT'S ALREADY SORTED IN NON-DECREASING ORDER.FIND TWO NUMBERS SUCH THAT THEY ADD UP TO A SPECIFIC "TARGET" NUMBERS. LET THESE TWO NUMBERS BE NUMBERS[INDEX1] AND NUMBERS[INDEX2] WHERE 1<=INDEX1<INDEX2<=NUMBERS.LENGTH
+
+//Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array[index1,index2] of length2.
+//the tests are generated such that there is exactly one solution you may not use the same length element twice
+
+//e.g., INPUT: numbers=[2,7,11,15], target=9
+//OUTPUT: [1,2] index
+//Explanation: The sum of 2 and 7 is 9 therefore, index1=1,index2=2 we return 
 
 #include<iostream>
 #include<vector>
@@ -9,41 +14,36 @@ using namespace std;
 
 class solution{
     public:
-    vector<int>twosum(vector<int>& numbers, int target){
-        // //for loop for iteration 
-        // for(int i=0;i<numbers.size();i++){
-        //     if(numbers[i]+=target)
-        //     return target;
+    vector<int> twosum(vector<int>& numbers, int target){
 
         int left=0;
         int right=numbers.size()-1;
 
+        //while loop 
         while(left<right){
             int sum=numbers[left]+numbers[right];
 
             if(sum==target){
-                //bcz we add plus one both side because due to indexing
-                return{left+1,right+1};
-            }
-            else if(sum<target){
+                return {left+1,right+1};
+            } else if(sum<target){
                 left++;
-
             } else{
                 right--;
             }
         }
-        return {};
-        }
-    };
+
+        return{};
+    }
+};
 
 //main function
 int main(){
-
     vector<int> numbers={2,7,11,15};
     int target=9;
 
     solution s;
     vector<int> ans=s.twosum(numbers,target);
-    cout<<ans[0]<<" "<<ans[1] ;
+    cout<<"["<<ans[0]<<","<<ans[1]<<"]";
+
     return 0;
 }
