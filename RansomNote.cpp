c4542c38301379b3d8a96eb358c2af7099fe2888
Jay Prakash Jha <prakashk60843@gmail.com>
2026-07-16 01:18:37 +0530

    Add files via upload

---
+++ RansomNote.cpp
@@ -0,0 +1,6 @@
+//383. RANSOM NOTE
+//Given two strings ransomeNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise
+//Each letter in magazine can only be used once in ransomNote.
+//e.g., 
+// INPUT : ransomNote = "a", magazine = "b"
+// OUTPUT : false
